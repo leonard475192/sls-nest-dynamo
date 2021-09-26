c757b3db2f27956898879d310a9e9b2979c24e00
leonard475192 <leonard.t1028@gmail.com>
2021-09-26 23:55:52 +0900

    :rocket: add:users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Req, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
+const API_KEY = '*****';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/users')
+  async createUser(@Body() body, @Req() request: Request): Promise<string> {
+    this.checkApiKey(request);
+    return await this.appService.createUser(body);
+  }
+
+  @Get('/users')
+  async getUsers(@Req() request: Request): Promise<string> {
+    this.checkApiKey(request);
+    return await this.appService.getUsers();
+  }
+
+  private checkApiKey(request: Request): void {
+    if ((request.headers['x-api-key'] as string) !== API_KEY) {
+      throw new Error('API KEY ERROR');
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { AppRepository } from './app.repository';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async createUser(body): Promise<string> {
+    if (!body || !body.name) {
+      return JSON.stringify({ error: 'name は必須項目です' });
+    }
+    const result = await new AppRepository().create(body);
+    return JSON.stringify(result.Attributes);
+  }
+
+  async getUsers(): Promise<string> {
+    const result = await new AppRepository().getAll();
+    return JSON.stringify(result.Items);
   }
 }
